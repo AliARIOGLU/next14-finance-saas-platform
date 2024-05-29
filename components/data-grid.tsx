727c8512bd91ec8f05a9bf44cb94629b5a79af1d
Ali <ali.arioglu06@gmail.com>
2024-05-29 13:51:46 +0300

    dashboard cards & summary api

---
+++ components/data-grid.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
+
+import { formatDateRange } from "@/lib/utils";
+import { DataCard, DataCardLoading } from "@/components/data-card";
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+
+export const DataGrid = () => {
+  const params = useSearchParams();
+  const to = params.get("to") || undefined;
+  const from = params.get("from") || undefined;
+
+  const { data, isLoading } = useGetSummary();
+
+  const dateRangeLabel = formatDateRange({ to, from });
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+        <DataCardLoading />
+        <DataCardLoading />
+        <DataCardLoading />
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        dateRange={dateRangeLabel}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
+        dateRange={dateRangeLabel}
+      />
+    </div>
+  );
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+import { eachDayOfInterval, isSameDay, subDays, format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -19,4 +20,88 @@ export function formatCurrency(value: number) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(value);
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0,
+      };
+    }
+  });
+
+  return transactionsByDay;
+}
+
+type Period = {
+  from: string | Date | undefined;
+  to: string | Date | undefined;
+};
+
+export function formatDateRange(period?: Period) {
+  const defaultTo = new Date();
+  const defaultFrom = subDays(defaultTo, 30);
+
+  if (!period?.from) {
+    return `${format(defaultFrom, "LLL dd")} - ${format(
+      defaultTo,
+      "LLL dd, y"
+    )}`;
+  }
+
+  if (period.to) {
+    return `${format(period.from, "LLL dd")} - ${format(
+      period.to,
+      "LLL dd, y"
+    )}`;
+  }
+
+  return format(period.from, "LLL dd, y");
+}
+
+export function formatPercentage(
+  value: number,
+  options: { addPrefix?: boolean } = { addPrefix: false }
+) {
+  const result = new Intl.NumberFormat("en-US", {
+    style: "percent",
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+
+  return result;
 }
